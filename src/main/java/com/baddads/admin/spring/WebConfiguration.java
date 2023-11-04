3ce2bbc47bd0956477af9c6981787d1f8867c4e0
Dylan Bolger <dylan.bolger00@gmail.com>
2023-11-04 16:28:30 -0500

    add actual corsmapping

--- src/main/java/com/baddads/admin/spring/WebConfiguration.java
+++ src/main/java/com/baddads/admin/spring/WebConfiguration.java
@@ -1,2 +1,14 @@
-package com.baddads.admin.spring;public class WebConfiguration {
+package com.baddads.admin.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
 }
