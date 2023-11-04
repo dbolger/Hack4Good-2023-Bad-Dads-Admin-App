6cd7e124e1065f255a8814517b07c9d89a79d9ce
Dylan Bolger <dylan.bolger00@gmail.com>
2023-11-03 23:08:49 -0500

    Add compose file, add build and build-deploy scripts, add some application.properties

---
+++ build.sh
@@ -0,0 +1,5 @@
+# Build project
+./gradlew build
+
+# Build docker image
+docker build -t baddads/admin .
--- src/main/java/com/baddads/controllers/user/UserController.java
+++ src/main/java/com/baddads/controllers/user/UserController.java
@@ -1,12 +1,12 @@
 package com.baddads.controllers.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @RequestMapping("/ping")
+    @GetMapping("/ping")
     public String ping() {
         return "Pong!";
     }
