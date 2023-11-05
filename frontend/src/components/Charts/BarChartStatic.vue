cdd18177eed45e43205fcaaad61aefb889c6f1ae
Daniel Moore <dmoore1987@gmail.com>
2023-11-05 01:13:04 -0500

    more graphs

---
+++ frontend/src/components/Charts/BarChartStatic.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+const barData = {
+  labels: ['Good Dads', 'Slipping Dads', 'Progressing Dads', 'Bad Dads'],
+  datasets: [
+    {
+      label: 'Dads',
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      data: [40, 20, 80, 10]
+    }
+  ]
+}
+const barOptions = {
+  responsive: false,
+  maintainAspectRatio: false
+}
+</script>
+<template>
+  <div style="position: relative">
+    <bar :data="barData" :options="barOptions" />
+  </div>
+</template>
--- frontend/src/pages/Reports.vue
+++ frontend/src/pages/Reports.vue
@@ -1,8 +1,10 @@
 <script setup>
 import DoughnutChartStatic from '../components/Charts/DoughnutChartStatic.vue'
+import BarChartStatic from '../components/Charts/BarChartStatic.vue'
+import ChartStatic from '../components/Charts/ChartStatic.vue'
 </script>
 <template>
-    <h1>Reports</h1>
-    <doughnut-chart-static />
-    <p>This is silly</p>
+  <h1>Reports</h1>
+  <doughnut-chart-static />
+  <bar-chart-static />
 </template>
